57a6bfa121b2b586ded976a96ff02b2a75432fdb
ParkerRailton <parker.railton@gmail.com>
2025-12-18 17:47:26 -0500

    valid move logic is in minimal viable form

--- src/chess/backend/GameManager.java
+++ src/chess/backend/GameManager.java
@@ -4,6 +4,8 @@ import chess.frontend.Client;
 import chess.utilities.ChessUtil.TurnType;
 
 import java.util.ArrayList;
+import java.awt.Point;
+import java.util.LinkedList;
 
 public class GameManager {
     private ChessBoard board = new ChessBoard();
@@ -46,15 +48,16 @@ public class GameManager {
 
     //TODO actually make this shitty fucking method.
     public boolean validMove(int x1, int y1, int x2, int y2) {
+        if (x1-x2 == 0 && y1-y2 == 0) return false;
         char p = board.getPiece(x1, y1);
         char t = board.getPiece(x2, y2);
         return switch (Character.toLowerCase(p)) {
             case 'p' -> pawnValid(x1, y1, x2, y2, p, t);
-            case 'n' -> knightValid(x1, y1, x2, y2);
-            case 'b' -> bishopValid(x1, y1, x2, y2);
-            case 'r' -> rookValid(x1, y1, x2, y2);
-            case 'q' -> queenValid(x1, y1, x2, y2);
-            case 'k' -> kingValid(x1, y1, x2, y2);
+            case 'n' -> knightValid(x1, y1, x2, y2, p, t);
+            case 'b' -> bishopValid(x1, y1, x2, y2, p, t);
+            case 'r' -> rookValid(x1, y1, x2, y2, p, t);
+            case 'q' -> queenValid(x1, y1, x2, y2, p, t);
+            case 'k' -> kingValid(x1, y1, x2, y2, p, t);
             default -> false;
         };
     }
@@ -70,9 +73,9 @@ public class GameManager {
     private boolean pawnValid(int x1, int y1, int x2, int y2, char p, char t) {
         if (t == ' ') {
             if (Character.isUpperCase(p)) {
-                return x2 == x1 && y2 == y1 + 1;
-            } else {
                 return x2 == x1 && y2 == y1 - 1;
+            } else {
+                return x2 == x1 && y2 == y1 + 1;
             }
 
         } else {
@@ -88,27 +91,80 @@ public class GameManager {
     }
 
 
-private boolean knightValid(int x1, int y1, int x2, int y2) {
+private boolean knightValid(int x1, int y1, int x2, int y2, char p, char t) {
+        if (t != ' ' && !isEnemy(p, t)) return false;
+    return ((x2 == x1 + 1 || x2 == x1 - 1) && (y2 == y1 + 2 || y2 == y1 - 2)) ||
+            ((x2 == x1 + 2 || x2 == x1 - 2) && (y2 == y1 + 1 || y2 == y1 - 1));
+}
+
+private boolean bishopValid(int x1, int y1, int x2, int y2, char p, char t) {
+    if (t != ' ' && !isEnemy(p, t)) return false;
+    //check if move is on the diagonal
+    int dX = x2-x1;
+    int dY = y2-y1;
+    if (Math.abs(dY) != Math.abs(dX)) return false;
+
+    //check for pieces in the way
+    int sigX = (int) Math.signum(dX);
+    int sigY = (int) Math.signum(dY);
+    for (int i = 1; i < Math.abs(dX); i++) {
+        if (board.getPiece(x1 + i*sigX, y1 +i*sigY) != ' ') return false;
+    }
     return true;
 }
 
-private boolean bishopValid(int x1, int y1, int x2, int y2) {
+private boolean rookValid(int x1, int y1, int x2, int y2, char p, char t) {
+    if (t != ' ' && !isEnemy(p, t)) return false;
+    //check if move is on the cardinal
+    int dX = x2-x1;
+    int dY = y2-y1;
+    if (dX != 0 && dY != 0) return false;
+
+    //check for pieces in the way
+    int sigX = (int) Math.signum(dX);
+    int sigY = (int) Math.signum(dY);
+    for (int i = 1; i < Math.max(Math.abs(dX), Math.abs(dY)); i++) {
+        if (board.getPiece(x1 + i*sigX, y1 +i*sigY) != ' ') return false;
+    }
     return true;
 }
 
-private boolean rookValid(int x1, int y1, int x2, int y2) {
+private boolean queenValid(int x1, int y1, int x2, int y2, char p, char t) {
+    if (t != ' ' && !isEnemy(p, t)) return false;
+    //check if move is on the cardinal or diagonal
+    int dX = x2-x1;
+    int dY = y2-y1;
+    if ((dX != 0 && dY != 0) && (Math.abs(dY) != Math.abs(dX)))return false;
+
+    //check for pieces in the way
+    int sigX = (int) Math.signum(dX);
+    int sigY = (int) Math.signum(dY);
+    for (int i = 1; i < Math.max(Math.abs(dX), Math.abs(dY)); i++) {
+        if (board.getPiece(x1 + i*sigX, y1 +i*sigY) != ' ') return false;
+    }
     return true;
 }
-
-private boolean queenValid(int x1, int y1, int x2, int y2) {
-    return true;
-}
-
-private boolean kingValid(int x1, int y1, int x2, int y2) {
+//TODO prevent moving into check
+private boolean kingValid(int x1, int y1, int x2, int y2, char p, char t) {
+    if (t != ' ' && !isEnemy(p, t)) return false;
+    int dX = x2-x1;
+    int dY = y2-y1;
+    if (Math.abs(dX) > 1 || Math.abs(dY) > 1) return false;
     return true;
 }
 
 //endregion
 
+    public LinkedList<Point> allValidMoves(int x, int y) {
+        LinkedList<Point> list = new LinkedList<>();
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (validMove(x, y, j, i)) {
+                    list.add(new Point(j, i));
+                }
+            }
+        }
+        return list;
+    }
 
 }
